cc4a9708ea13bcb58e47f2210784427f09d111fe
AnneAriel Ceyrac <anneariel.ceyrac@gmail.com>
2022-11-29 11:55:04 +0100

    debug

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :matches do
     resources :wines do
-      resources :wishlist
+      resources :bookmarks
     end
     resources :dishes, except: :index
   end
