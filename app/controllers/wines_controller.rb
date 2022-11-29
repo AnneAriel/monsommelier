818aaff8e0e1a8d2b82799a06561a68a55877af8
jean-louis spatuzza <jlspatuzza@hotmail.fr>
2022-11-29 11:35:48 +0100

    winecontrol

---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,49 @@
+class WinesController < ApplicationController
+
+  def index
+
+    # @wines = policy_scope(Wine)
+
+    # if params[:query].present?
+    #   @wines = Bike.search_by_wine_criteria(params[:query])
+    # else
+
+    @bikes = Bike.all
+
+  end
+
+def show
+  @wine = Wine.find(params[:id])
+  # authorize @wine
+end
+
+def new
+  @wine = Wine.new
+  # authorize @wine
+end
+
+def create
+  @wine = Wine.new(wine_params)
+    @wine.user = current_user
+    @wine.save
+    redirect_to wine_path(@wine)
+    authorize @wine
+end
+
+def destroy
+  @wine = Wine.find(params[:id])
+  authorize @wine
+  @wine.destroy
+  redirect_to wines_path
+end
+
+
+private
+
+  def wine_params
+    params.require(:wine).permit(:domaine, :appellation, :category, :daily_price, :photo)
+  end
+
+
+
+end
