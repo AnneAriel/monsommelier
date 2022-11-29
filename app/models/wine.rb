f6e297cb345cdf48c13f6230f8de1ca9eb96aee9
AnneAriel Ceyrac <anneariel.ceyrac@gmail.com>
2022-11-29 11:59:13 +0100

    has_many

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,7 @@
 class Wine < ApplicationRecord
   has_many :comments, as: :commented_on
-  has_many :matches, :bookmarks
+  has_many :matches
+  has_many :bookmarks
   validates :appellation, presence: true
   validates :couleur, presence: true
 end
