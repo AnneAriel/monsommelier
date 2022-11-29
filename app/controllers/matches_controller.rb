a592afeed659c7e5708122e6d51147436a8bbe0b
Nelbi Leon <nelbi.leon@gmail.com>
2022-11-29 11:27:38 +0100

    started matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,15 +1,23 @@
 class MatchesController < ApplicationController
-
+before_action
   # def index
   # end
 
   def show
+    @match = Match.find(params[:id])
   end
 
   def new
+    @match = Match.new
   end
 
   def create
+    @match = Match.new(match_params)
+      if @match.save
+        redirect_to match_wine_path(@match)
+      else
+        render :new, status: :unprocessable_entity
+      end
   end
 
   def edit
@@ -19,6 +27,12 @@ class MatchesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:wine_id, :dish_id)
   end
 
 end
