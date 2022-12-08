b289994d1241752d5a99092292ce69b3af1b45c8
AnneAriel Ceyrac <anneariel.ceyrac@gmail.com>
2022-12-08 12:56:33 +0100

    ajustement

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ApplicationRecord
 
 
   include PublicActivity::Model
-  tracked only: [:create], owner: Proc.new{ |controller, model| controller.current_user }
+  # tracked only: [:create], owner: Proc.new{ |controller, model| controller.current_user }
 
 
 
